70c29ad43ca680f5857d8b6ea4792eeddd053a3a
Swara-Joshi <swara090903@gmail.com>
2025-03-08 15:28:59 -0500

    tools and llms setup

--- ai_agent.py
+++ ai_agent.py
@@ -5,8 +5,15 @@ GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
 TAVILY_API_KEY = os.environ.get('TAVILY_API_KEY')
 OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
 
-#Step2: LLM & Tools 
+#Step2: Setup LLM & Tools 
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
 
+openai_llm=ChatOpenAI(model="gpt-4o-mini")
+groq_llm=ChatGroq(model="llama-3.3-70b-versatile")
+
+search_tool=TavilySearchResults(max_results=2)
 
 #Step3: Setup AI Agent with search functionality
 
