0c510fee1a17a561ea7a6d672bdf70dc8f88e25a
Swara-Joshi <swara090903@gmail.com>
2025-03-08 15:16:03 -0500

    setting up api keys of groq openai and tavily

---
+++ ai_agent.py
@@ -0,0 +1,12 @@
+#Step1: Setup of api key with groq, OPEN AI and tavily
+import os
+
+GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
+TAVILY_API_KEY = os.environ.get('TAVILY_API_KEY')
+OPEN_AI_API_KEY = os.environ.get('OPEN_AI_API_KEY')
+
+#Step2: LLM & Tools 
+
+
+#Step3: Setup AI Agent with search functionality
+
